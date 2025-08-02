2f968466999a03ffb3f8a762d8b5787ef3a3e5f4
JafRojasCR <jafetalonsorb08@gmail.com>
2025-08-02 15:18:36 -0600

    Converted the system into a basic PWA

---
+++ public/service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'mi-pwa-cache-v1';
+const urlsToCache = [
+  '/', // o tu start_url
+  '/login.html',
+  '/registro.html',
+  '/css/loginreg.css',
+  '/css/camiseta.css',
+  '/css/header.css',
+  '/camiseta.html',
+  '/manifest.json', // Asegúrate de tener un manifest.json];
+];
+// Evento 'install': se dispara cuando el service worker es instalado
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caché abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Evento 'fetch': intercepta peticiones de la red
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Devuelve el recurso de la caché si está disponible
+        if (response) {
+          return response;
+        }
+        // Si no está en la caché, continúa con la petición de red
+        return fetch(event.request);
+      })
+  );
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,8 +3,8 @@ const path = require('path');
 const router = express.Router();
 const { verificarToken } = require('../middleware/auth'); // O la ubicación de tu middleware
 
-// Servir archivos estáticos del public:
-// router.use(express.static(path.join(__dirname, '../public')));
+// Servir archivos estáticos del public: (PARA PRUEBAS DE PWA)
+router.use(express.static(path.join(__dirname, '../public')));
 
 // Endpoints para que al ingresar algo como "localhost:3000/registro.html" no sirva, pero que al
 // poner cosas como "localhost:3000/registro" sirva el archivo registro.html
@@ -16,6 +16,10 @@ res.sendFile(path.join(__dirname, '../public', 'camiseta.html')); // Envía el a
 
 router.get('/registro', (req,res) => {
 res.sendFile(path.join(__dirname, '../public', 'registro.html')); // Envía el archivo registro.html
+});
+
+router.get('/', (req,res) => {
+res.sendFile(path.join(__dirname, '../public', 'login.html')); // Envía el archivo login.html
 });
 
 router.get('/login', (req,res) => {
