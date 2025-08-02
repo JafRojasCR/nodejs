b943881d71eeeb00949b2afcd4e80345d8a1733d
JafRojasCR <jafetalonsorb08@gmail.com>
2025-08-02 14:05:13 -0600

    TESTING REVERTS Reapply "Oganized code sections in a better way: middleware (endpoints) for serving files are now located in their own file, the database connection is served in an independent file"
    This reverts commit dbe3787ccb8c4db3e65a9230c430331d337defba.

--- app.js
+++ app.js
@@ -1,68 +1,26 @@
-require('dotenv').config();
+require('dotenv').config();   // Cargar las variables de entorno desde el archivo .env
 const userRoutes = require('./routes/usuarioRutas');    // Importa las rutas de usuario
 const camisaRoutes = require('./routes/camisaRutas'); // Importa las rutas de camiseta
+const viewRoutes = require('./routes/viewRoutes'); // Importa las rutas de vistas
 const express = require('express');
-const mongoose = require('mongoose'); // importamos la librería Mongoose 
+const mongoose = require('mongoose');
 const path = require('path'); // Módulo para rutas absolutas
 const PORT = process.env.PORT;
 const { verificarToken } = require('./middleware/auth'); // Importa el middleware de autenticación
+const connectDB = require('./config/db'); // Importa la función de conexión a la base de datos
 
+connectDB(); // Llama a la función para conectar a la base de datos
 
-// URI de conexión a MongoDB (MongoDB Atlas en este caso).  
-// Reemplaza <usuario>, <password> y <tuBase> por tus datos reales. 
-const mongoURI = process.env.MONGO_URI; 
-// Opciones recomendadas para evitar advertencias (según la versión de Mongoose) 
-const options = { 
-useNewUrlParser: true,    // Usa el nuevo parser de URL Mongo 
-useUnifiedTopology: true  // Usa el nuevo motor de manejo de topologías 
-}; 
-
-// Conectarse a la base de datos: 
-mongoose.connect(mongoURI, options) 
-.then(() => console.log('Conectado a MongoDB Atlas'))   // Éxito en la conexión 
-.catch(err => console.error('Error de conexión:', err)); // Manejo de error 
-
-
+// Inicializa la aplicación Express
 const app = express();
 
 // Middleware para parsear JSON en las peticiones (body-parser integrado)
 app.use(express.json());   // Permite recibir datos en formato JSON
-// app.use(express.static(path.join(__dirname, 'public')));
-// Esta que está comentada es para servir archivos estáticos desde la carpeta public, entonces se
-// puede acceder desde el navegador, como: localhost:3000/registro.html
 
 
-// Endpoints para que al ingresar algo como "localhost:3000/registro.html" no sirva, pero que al
-// poner cosas como "localhost:3000/registro" sirva el archivo registro.html
-
-app.get('/camiseta', verificarToken, (req,res) => {
-res.sendFile(path.join(__dirname, 'public', 'camiseta.html')); // Envía el archivo camiseta.html
-});
-
-
-app.get('/registro', (req,res) => {
-res.sendFile(path.join(__dirname, 'public', 'registro.html')); // Envía el archivo registro.html
-});
-
-app.get('/login', (req,res) => {
-res.sendFile(path.join(__dirname, 'public', 'login.html')); // Envía el archivo login.html
-});
-
-app.get('/css/loginreg', (req,res) => {
-res.sendFile(path.join(__dirname, 'public', 'css', 'loginreg.css')); // Envía el archivo loginreg.css
-});
-
-app.get('/css/camiseta', (req,res) => {
-res.sendFile(path.join(__dirname, 'public', 'css', 'camiseta.css')); // Envía el archivo camiseta.css
-});
-
-app.get('/css/header', (req,res) => {
-res.sendFile(path.join(__dirname, 'public', 'css', 'header.css')); // Envía el archivo header.css
-});
-
-  // Todo lo de la carpeta public se sirve de forma estática y accesible desde fuera (navegador)
 app.use('/api/usuarios', userRoutes); // Usa las rutas de usuario definidas en usuarioRutas.js
 app.use('/api/camisetas', camisaRoutes); // Usa las rutas de camiseta definidas en camisetaRutas.js
+app.use('/', viewRoutes); // Usa las rutas de vistas definidas en viewRoutes.js
 app.listen(PORT, () => {
   console.log(`Servidor API escuchando en http://localhost:${PORT}`);
 });
